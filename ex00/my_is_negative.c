0127131f3f13ef756538b69a7d323426393bbc45
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-04-23 22:44:40 +0000

    Added calculation of absolute value

---
+++ ex00/my_is_negative.c
@@ -0,0 +1,21 @@
+/**************************************************************************************************
+*       My is Negative                                                                            *
+*       by Chansa Chishimba                                                                       *
+*       The program returns 1 or 0 depending on the integer's sign entered as a parameter         *
+**************************************************************************************************/
+
+
+int my_is_negative(int nbr)
+{
+    if (nbr < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+
+
---
+++ ex01/my_abs.c
@@ -0,0 +1,30 @@
+/**************************************************************************************************
+*       My absolute                                                                               *
+*       by Chansa Chishimba                                                                       *
+*       The program Reproduces behavior of an abs() function                                      *
+**************************************************************************************************/
+
+int main() 
+
+{
+    int my_abs (int a)
+    {
+        
+        //initialise the variables
+        a = 0;
+        b = 0;
+
+        
+
+        //process
+        (unsigned int) b == a ;
+    
+
+
+        //Output
+
+        printf("The absolute value of %d is %d \n", a,b)
+        return (0);
+
+    }
+}
