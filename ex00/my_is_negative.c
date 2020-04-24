c006ec4d7b0a545a71a08af6d812d9583f5f2491
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-04-24 02:50:17 +0000

    added tests

--- ex00/my_is_negative.c
+++ ex00/my_is_negative.c
@@ -4,6 +4,7 @@
 *       The program returns 1 or 0 depending on the integer's sign entered as a parameter         *
 **************************************************************************************************/
 
+//#include <stdio.h>
 
 int my_is_negative(int nbr)
 {
@@ -17,5 +18,16 @@ int my_is_negative(int nbr)
     }
 }
 
+/* Tests
+int main()
+{
+    
+    //  4 --> 1
+    // -4 --> 0
+    printf("My is negative : %d  \n", my_is_negative(4));
+    printf("My is negative : %d  \n", my_is_negative(0));
+    printf("My is negative : %d  \n", my_is_negative(-4));
 
+}
 
+*/
