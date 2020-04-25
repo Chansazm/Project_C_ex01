5eae7c7e2ad679eced43bd95c84217259038c0a5
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-04-25 20:00:46 +0000

    Added putchar

---
+++ ex08/my_print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void my_putchar(char c)
+{ 
+     write(1, &c, 1); 
+}
+
+void my_print_alphabet() 
+{
+    
+  for (char x = 'a'; x <= 'z'; x++)
+    my_putchar(x);
+}
+/*
+int main()
+{
+    my_print_alphabet();
+    
+}
+
+*/
