eaa13a8c9d46f14ba3da011e3dd0e4cc59a93370
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-04-25 15:01:51 +0000

    Added conditional for ex04

---
+++ ex04/my_isdigit.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+
+
+int my_isdigit(char a) 
+{
+if ((a >= 0) && (a <= 9))
+    return 1;
+else {
+return 0;
+}
+
+return 0;
+}
+
+
+/*
+int main()
+{
+    printf("The character %d is a digit: " ,  my_isdigit(0));
+}
+*/
