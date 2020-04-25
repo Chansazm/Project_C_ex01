2b849b43429ae23b2fc49d623220ad56c4e36dcb
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-04-25 15:51:01 +0000

    added is lower or not

---
+++ ex05/my_islower.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int my_islower(char a)
+{
+    if (a >= 'a' && a <= 'z')
+        return 1;
+    else {
+    return 0;
+    }
+return 0;
+  
+}
+/*
+int main()
+{
+    
+    //a --> 1
+    // A --> 0
+    //printf("The letter:%d is a small letter",my_islower(c));
+    printf("b is a small letter : %d  \n", my_islower('b'));
+
+
+    
+}
+*/
