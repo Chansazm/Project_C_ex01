53950f28e712ffebdf183b92c111b0a6a9951724
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-04-26 00:44:27 +0000

    Added new line

--- ex10/my_print_numbers.c
+++ ex10/my_print_numbers.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
+
+
+
 void my_putchar(char c)
 { 
      write(1, &c, 1); 
+     
 }
 
-*/
+
 
 void my_print_numbers() 
 {
     char x;
-    for (x = 0; x <= 9; x++)
-        //my_putchar(x);
-        printf("%d ", x);
+    for (x = '0'; x <= '9'; x++)
+        my_putchar(x);
+    
+    my_putchar('\n');
+        
+    
 }
-
+/*
 int main()
 {
     my_print_numbers();
+    
 }
+*/
